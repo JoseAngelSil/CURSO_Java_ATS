fac8125b06518a3da33453ca6a87a497bb28728f
Jose Angel <joseangel413silva@gmail.com!>
2022-11-02 22:29:23 -0600

    Add: Clase Math y modificacion de clase Operadores Aritmaticos
    Se modifico la clase Operadores aritmeticos sobre el uso de los
    operadores de la clase misma.
    Se crea la clase MAth para ver como se usa la clase Math para operadores
    matematicos especiales en Java, siendo que se va a consultar en la
    documentacion original de Java para ver cuantos metodos tiene esta clase

---
+++ src/Operadores/ClaseMath.java
@@ -0,0 +1,12 @@
+package Operadores;
+
+public class ClaseMath {
+    public static void main(String[] args) {
+        double raizCuadrada = Math.sqrt(46);
+        System.out.println("raizCuadrada = " + raizCuadrada);
+        double potencia = Math.pow(4, 3);
+        System.out.println("potencia = " + potencia);
+        double redondear = Math.round(raizCuadrada);
+        System.out.println("redondear = " + redondear);
+    }
+}
--- src/Operadores/OperadoresAritmaticos.java
+++ src/Operadores/OperadoresAritmaticos.java
@@ -1,7 +1,48 @@
 package Operadores;
 
+import java.util.Scanner;
+
 public class OperadoresAritmaticos {
     public static void main(String[] args) {
-        System.out.println("ingreso de datos aritmaticos en Java");
+        Scanner inputConsole = new Scanner(System.in);
+        float n1, n2, sum, res, mul,div;
+        System.out.print("Ingrese el valor de n1: ");
+        n1 = inputConsole.nextFloat();
+        System.out.print("Ingrese el valor de n2: ");
+        n2 = inputConsole.nextFloat();
+        sum = n1 + n2;
+        res = n1 - n2;
+        mul = n1 * n2;
+        div = n1 / n2;
+        System.out.println("La suma es " + sum);
+        System.out.println("La resta es " + res);
+        System.out.println("La multi es " + mul);
+        System.out.println("La div es " + div);
+
+
+        //Operadores combinados
+        System.out.println("Operadores convinados");
+        int n = 5;
+        n ++;
+        System.out.println("n = " + n);
+        n += 15;
+        System.out.println("n = " + n);
+        n *= 2;
+        System.out.println("n = " + n);
+        n %= 3;
+        System.out.println("n = " + n);
+        
+        //Operador de incremento y decremento
+        System.out.println("Incremento y decremento");
+        int m = 5;
+        System.out.println("m = " + m);
+        m ++;
+        System.out.println("m = " + m);
+        ++m;
+        System.out.println("m = " + m);
+        --m;
+        System.out.println("m = " + m);
+        m --;
+        System.out.println("m = " + m);
     }
 }
